ed0e003654641ee43853f225eaced5ea8b6dcf2a
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2024-02-21 19:28:33 +0100

    add calculator functions

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,21 +1,21 @@
 import { calculator } from '../index.js'
 
-test("calculator exists", () => {
-  expect(calculator).toEqual({});
-});
-
 test("calculator adds two values", () => {
   expect(calculator.add(2, 2)).toEqual(4);
 });
 
 test("calculator subtracts two values", () => {
-  expect(calculator.add(2, 2)).toEqual(0);
+  expect(calculator.subtract(2, 2)).toEqual(0);
 });
 
 test("calculator multiplies two values", () => {
-  expect(calculator.add(3, 3)).toEqual(9);
+  expect(calculator.multiply(3, 3)).toEqual(9);
 });
 
 test("calculator divides two values", () => {
-  expect(calculator.add(5, 2)).toBeCloseTo(2.5);
+  expect(calculator.divide(5, 2)).toBeCloseTo(2.5);
+});
+
+test("calculator handles divide by zero", () => {
+  expect(() => calculator.divide(5, 0)).toThrow(/by zero/);
 });
