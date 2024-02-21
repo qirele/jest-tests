960aa93919c4b398bec8a56ce894b572d8cbde4c
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2024-02-21 21:36:37 +0100

    add analazyeArray tests & implementation

--- index.js
+++ index.js
@@ -44,3 +44,12 @@ export function nextLetter(letter, key) {
 
   return letter;
 }
+
+export function analyzeArray(arr) {
+  return {
+    average: arr.reduce((prev, cur) => prev + cur, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,11 @@
+import { analyzeArray } from '../index.js'
+
+
+test("analyzeArray returns avg, min, max and length", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  });
+});
